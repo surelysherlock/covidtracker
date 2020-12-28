62cce23e95d65e562be4979a3ecacb5055684905
Sanyan Sharar <raad34@gmail.com>
2020-12-28 12:08:04 -0500

    Added form to TS link

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,13 +9,34 @@ import { FormGroup, FormControl} from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   stateData: any;
+
   formatedStateData: any;
+
   monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"];
+
   form = new FormGroup({
-    state: new FormControl()
+    stateForm: new FormControl()
   })
+
+  state = new FormControl();
+
+  updateState(data:any) {
+    this.state.setValue("cool");
+  }
+
+  get first(): any {
+    return this.form.get('stateForm');
+  }
+
   
+  stateName: String = "None";
+  totalPositiveCases: String = "totalPositiveCases";
+  lastUpdatedDate: String = "lastUpdatedDate";
+  currentlyHospitalized: String = "currentlyHospitalized";
+  numberOfCasesToday: String = "numberOfCasesToday";
+  numberOfHospitalizedToday: String = "numberOfHospitalizedToday";
+
   //Formats dates
   modifyDateForResponse(data: any){
     data.forEach((value: any) => {
@@ -33,10 +54,19 @@ export class HomeComponent implements OnInit {
     return data;
   }
 
-  getCurrentData(){
+  returnStateName(data: any){
+   return data[0].state
+  }
+
+  getCurrentData(stateName:String){
+    // console.log(this.form.get('stateForm')?.value);
+    console.log(stateName);
+    
     this.http.get('https://api.covidtracking.com/v1/states/current.json').subscribe(response => {
     this.stateData = response;
     this.stateData = this.modifyDateForResponse(this.stateData);
+    this.stateName = this.returnStateName(this.stateData);
+    this.state.setValue(this.stateName);
     // this.stateData.forEach((element:any) => {
     //   console.log(element.state)
     // });
