8aedf328b3cada59c40272dccb14a20518c5b15e
Sanyan Sharar <raad34@gmail.com>
2020-12-31 15:46:11 -0500

    Added route to empty route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent }
 ];
 
