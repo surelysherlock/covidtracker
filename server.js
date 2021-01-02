3366babc669ab2074a79bc3acf9a3c9560290bd5
Sanyan Sharar <raad34@gmail.com>
2021-01-01 22:05:46 -0500

    base href to /

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/covid-tracker'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/covid-tracker/index.html'));});
+res.sendFile(path.join(__dirname +  '/dist/covid-tracker/index.html'));});
 app.listen(process.env.PORT || 8080);
