580aaa0265628477ea52bc2696496352e4d0ca14
Sanyan Sharar <raad34@gmail.com>
2021-03-15 12:53:30 -0400

    ADDED ability to look data with full state name

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { CovidTrackerService } from 'src/app/restapi.service';
 
 @Component({
@@ -8,8 +8,8 @@ import { CovidTrackerService } from 'src/app/restapi.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  
-  constructor(private covidService: CovidTrackerService) { 
+
+  constructor(private covidService: CovidTrackerService) {
 
   }
   stateData: any;
@@ -22,11 +22,73 @@ export class HomeComponent implements OnInit {
   numberOfHospitalizedToday: String = "N/A";
 
   monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"];
+    "July", "August", "September", "October", "November", "December"];
 
   form = new FormGroup({
     stateForm: new FormControl()
   })
+
+  nameOfStates: object = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AS": "American Samoa",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "District Of Columbia",
+    "FM": "Federated States Of Micronesia",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "GU": "Guam",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MH": "Marshall Islands",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "MP": "Northern Mariana Islands",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PW": "Palau",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+  }
 
 
   // get first(): any {
@@ -34,47 +96,55 @@ export class HomeComponent implements OnInit {
   // }
 
   //Formats dates
-  modifyDateForResponse(data: any){
+  modifyDateForResponse(data: any) {
     data.forEach((value: any) => {
       let yearOfDate: string;
       let monthOfDate: string;
       let dateOfDate: string;
       let monthOfDateInNumber: number;
-      yearOfDate = value.date.toString().substr(0,4);
-      monthOfDate= value.date.toString().substr(4,2);
+      yearOfDate = value.date.toString().substr(0, 4);
+      monthOfDate = value.date.toString().substr(4, 2);
       dateOfDate = value.date.toString().substr(6);
       monthOfDateInNumber = parseInt(monthOfDate);
-      monthOfDate = this.monthNames[monthOfDateInNumber-1];
+      monthOfDate = this.monthNames[monthOfDateInNumber - 1];
       value.date = monthOfDate + " " + dateOfDate + " " + yearOfDate;
-    } );
+    });
     return data;
   }
 
-  getCurrentData(stateNameInput:String){
+  getCurrentData(stateNameInput: String) {
     this.covidService.getStateWiseData().subscribe(response => {
-    this.stateData = response;
-    this.stateData = this.modifyDateForResponse(this.stateData);
-    for (let i = 0; i < this.stateData.length; i++) {
-      const stateNameHolder = this.stateData[i]['state'];
-      if (stateNameHolder == stateNameInput) {
-        this.stateData = this.stateData[i];
+      this.stateData = response;
+      this.stateData = this.modifyDateForResponse(this.stateData);
+      for (let i = 0; i < this.stateData.length; i++) {
+        const stateNameHolder = this.stateData[i]['state'];
+        if (stateNameInput.length > 2) {
+          for (const [key, value] of Object.entries(this.nameOfStates)) {
+            if (stateNameInput == value) {
+              stateNameInput = key
+            }
+          }
+        }
+        if (stateNameHolder == stateNameInput.toUpperCase()) {
+          this.stateData = this.stateData[i];
+        }
       }
-    }
-    this.stateName = this.stateData['state'];
-    this.totalPositiveCases = this.stateData['positive'];
-    this.lastUpdatedDate = this.stateData['date'];
-    this.currentlyHospitalized = this.stateData['hospitalizedCurrently'];
-    this.numberOfCasesToday = this.stateData['positiveIncrease'];
-    this.numberOfHospitalizedToday = this.stateData['hospitalizedIncrease'];
-    this.lastUpdatedDate = this.stateData['date'];
+
+      this.stateName = this.stateData['state'];
+      this.totalPositiveCases = this.stateData['positive'];
+      this.lastUpdatedDate = this.stateData['date'];
+      this.currentlyHospitalized = this.stateData['hospitalizedCurrently'];
+      this.numberOfCasesToday = this.stateData['positiveIncrease'];
+      this.numberOfHospitalizedToday = this.stateData['hospitalizedIncrease'];
+      this.lastUpdatedDate = this.stateData['date'];
     })
   }
 
 
-  
-  
+
+
   ngOnInit(): void {
-  
+
   }
 
 }
